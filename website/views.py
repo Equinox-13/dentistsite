77df0da0206dd5188004f1efa33c985bba77a3cb
Equinox-13 <quaid.johar33@gmail.com>
2022-06-13 23:03:02 +0530

    [IMP] Modify contactus form, Made changes in contactus view

--- website/views.py
+++ website/views.py
@@ -4,4 +4,15 @@ def home(request):
     return render(request, 'home.html', {})
 
 def contact(request):
-    return render(request, 'contact.html', {})
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_phone = request.POST['message-phone']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+        print("message_name==========>", message_name )
+        print("message_phone==========>", message_phone )
+        print("message_email==========>", message_email )
+        print("message==========>", message )
+        return render(request, 'contact.html', {'message_name': message_name})
+    else:
+        return render(request, 'contact.html', {})
